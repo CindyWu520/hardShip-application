d166ca36b1eaf0ecbd37f5d702dc21dcfcdfa858
cindyWu <xiangyanwu1122@gmail.com>
2026-04-16 08:57:59 +1000

    initial set up

---
+++ src/test/java/com/assignment/hardship/HardshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.hardship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
