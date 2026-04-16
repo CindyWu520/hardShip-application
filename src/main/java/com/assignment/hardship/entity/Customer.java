cc03ae8887ebcc0c77ff9eeea5cd1ca3451bcbec
cindyWu <xiangyanwu1122@gmail.com>
2026-04-16 12:45:08 +1000

    feat: entity

---
+++ src/main/java/com/assignment/hardship/entity/Customer.java
@@ -0,0 +1,2 @@
+package com.assignment.hardship.entity;public class User {
+}
---
+++ src/main/java/com/assignment/hardship/entity/HardShip.java
@@ -0,0 +1,17 @@
+package com.assignment.hardship.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "hard_ship")
+public class HardShip {
+
+
+}
